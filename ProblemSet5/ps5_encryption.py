ecdc24e082586c0c9c227fe7d5d5d00fefa1fcbb
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-03-05 01:40:47 +0100

    add problem set 5

---
+++ ProblemSet5/ps5_encryption.py
@@ -0,0 +1,200 @@
+# 6.00x Problem Set 5
+#
+# Part 1 - HAIL CAESAR!
+
+import string
+import random
+
+WORDLIST_FILENAME = "words.txt"
+
+
+# -----------------------------------
+# Helper code
+# (you don't need to understand this helper code)
+def loadWords():
+    """
+    Returns a list of valid words. Words are strings of lowercase letters.
+
+    Depending on the size of the word list, this function may
+    take a while to finish.
+    """
+    print "Loading word list from file..."
+    inFile = open(WORDLIST_FILENAME, 'r')
+    wordList = inFile.read().split()
+    print "  ", len(wordList), "words loaded."
+    return wordList
+
+
+def isWord(wordList, word):
+    """
+    Determines if word is a valid word.
+
+    wordList: list of words in the dictionary.
+    word: a possible word.
+    returns True if word is in wordList.
+
+    Example:
+    >>> isWord(wordList, 'bat') returns
+    True
+    >>> isWord(wordList, 'asdf') returns
+    False
+    """
+    word = word.lower()
+    word = word.strip(" !@#$%^&*()-_+={}[]|\\:;'<>?,./\"")
+    return word in wordList
+
+
+def randomWord(wordList):
+    """
+    Returns a random word.
+
+    wordList: list of words
+    returns: a word from wordList at random
+    """
+    return random.choice(wordList)
+
+
+def randomString(wordList, n):
+    """
+    Returns a string containing n random words from wordList
+
+    wordList: list of words
+    returns: a string of random words separated by spaces.
+    """
+    return " ".join([randomWord(wordList) for _ in range(n)])
+
+
+def randomScrambled(wordList, n):
+    """
+    Generates a test string by generating an n-word random string
+    and encrypting it with a sequence of random shifts.
+
+    wordList: list of words
+    n: number of random words to generate and scamble
+    returns: a scrambled string of n random words
+
+    NOTE:
+    This function will ONLY work once you have completed your
+    implementation of applyShifts!
+    """
+    s = randomString(wordList, n) + " "
+    shifts = [(i, random.randint(0, 25)) for i in range(len(s)) if s[i-1] == ' ']
+    return applyShifts(s, shifts)[:-1]
+
+
+def getStoryString():
+    """
+    Returns a story in encrypted text.
+    """
+    return open("story.txt", "r").read()
+
+
+# (end of helper code)
+# -----------------------------------
+
+
+#
+# Problem 1: Encryption
+#
+def buildCoder(shift):
+    """
+    Returns a dict that can apply a Caesar cipher to a letter.
+    The cipher is defined by the shift value. Ignores non-letter characters
+    like punctuation, numbers and spaces.
+
+    shift: 0 <= int < 26
+    returns: dict
+    """
+    result = {}
+    for i in string.ascii_lowercase:
+        char = ord(i)+shift
+        if char > 122:
+            char = 96 + (char - 122)
+        result[i] = chr(char)
+
+    for i in string.ascii_uppercase:
+        char = ord(i)+shift
+        if char > 90:
+            char = 64 + (char - 90)
+        result[i] = chr(char)
+    return result
+
+
+def applyCoder(text, coder):
+    """
+    Applies the coder to the text. Returns the encoded text.
+
+    text: string
+    coder: dict with mappings of characters to shifted characters
+    returns: text after mapping coder chars to original text
+    """
+    text_new = []
+    for i in text:
+        if i in coder:
+            text_new.append(coder[i])
+        else:
+            text_new.append(i)
+    return ''.join(text_new)
+
+
+def applyShift(text, shift):
+    """
+    Given a text, returns a new text Caesar shifted by the given shift
+    offset. Lower case letters should remain lower case, upper case
+    letters should remain upper case, and all other punctuation should
+    stay as it is.
+
+    text: string to apply the shift to
+    shift: amount to shift the text (0 <= int < 26)
+    returns: text after being shifted by specified amount.
+    """
+    return applyCoder(text, buildCoder(shift))
+
+
+#
+# Problem 2: Decryption
+#
+def findBestShift(wordList, text):
+    """
+    Finds a shift key that can decrypt the encoded text.
+
+    text: string
+    returns: 0 <= int < 26
+    """
+    result = {}
+    for i in range(26):
+        text_new = applyShift(text, i)
+        result[i] = 0
+        for word in text_new.split(' '):
+            word = word.replace(',', '').replace('!', '')
+            if isWord(wordList, word):
+                result[i] += 1
+    return [n for n in result.keys() if result[n] == max(result.values())][0]
+
+
+def decryptStory():
+    """
+    Using the methods you created in this problem set,
+    decrypt the story given by the function getStoryString().
+    Use the functions getStoryString and loadWords to get the
+    raw data you need.
+
+    returns: string - story in plain text
+    """
+    story = getStoryString()
+    wordList = loadWords()
+    shift = findBestShift(wordList, story)
+    return applyShift(story, shift)
+
+#
+# Build data structures used for entire session and run encryption
+#
+
+if __name__ == '__main__':
+    # To test findBestShift:
+    wordList = loadWords()
+    s = applyShift('Hello, world!', 8)
+    bestShift = findBestShift(wordList, s)
+    assert applyShift(s, bestShift) == 'Hello, world!'
+    # To test decryptStory, comment the above four lines and uncomment this line:
+    #    decryptStory()
---
+++ ProblemSet5/ps5_recursion.py
@@ -0,0 +1,74 @@
+# 6.00x Problem Set 5
+#
+# Part 2 - RECURSION
+
+
+#
+# Problem 3: Recursive String Reversal
+#
+def reverseString(aStr):
+    """
+    Given a string, recursively returns a reversed copy of the string.
+    For example, if the string is 'abc', the function returns 'cba'.
+    The only string operations you are allowed to use are indexing,
+    slicing, and concatenation.
+
+    aStr: a string
+    returns: a reversed string
+    """
+    if len(aStr) < 2:
+        return aStr
+    else:
+        return aStr[-1]+reverseString(aStr[1:-1])+aStr[0]
+
+
+#
+# Problem 4: X-ian
+#
+def x_ian(x, word):
+    """
+    Given a string x, returns True if all the letters in x are
+    contained in word in the same order as they appear in x.
+
+    >>> x_ian('eric', 'meritocracy')
+    True
+    >>> x_ian('eric', 'cerium')
+    False
+    >>> x_ian('john', 'mahjong')
+    False
+
+    x: a string
+    word: a string
+    returns: True if word is x_ian, False otherwise
+    """
+    if len(x) == 0:
+        return True
+    elif x[0] in word:
+        return x_ian(x[1:], word[word.index(x[0]):])
+    else:
+        return False
+
+
+#
+# Problem 5: Typewriter
+#
+def insertNewlines(text, lineLength):
+    """
+    Given text and a desired line length, wrap the text as a typewriter would.
+    Insert a newline character ("\n") after each word that reaches or exceeds
+    the desired line length.
+
+    text: a string containing the text to wrap.
+    line_length: the number of characters to include on a line before wrapping
+        the next word.
+    returns: a string, with newline characters inserted appropriately.
+    """
+    def insertNewlinesRec(text, lineLength):
+        if len(text) <= lineLength:
+            return text
+        elif text[lineLength-1] == ' ':
+            return '%s\n%s' % (text[:lineLength], insertNewlinesRec(text[lineLength:], lineLength))
+        else:
+            return text[0] + insertNewlinesRec(text[1:], lineLength)
+
+    return insertNewlinesRec(text, lineLength)
