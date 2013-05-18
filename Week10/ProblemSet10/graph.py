f5d8ecd6d5790882f7c35857f4e24b1d6a106505
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2013-05-18 12:04:16 +0200

    add ProblemSet2 (without problem 4 yet)

---
+++ Week10/ProblemSet10/graph.py
@@ -0,0 +1,124 @@
+# 6.00x Problem Set 10
+# Graph optimization
+#
+# A set of data structures to represent graphs
+#
+
+
+class Node(object):
+    def __init__(self, name):
+        self.name = str(name)
+
+    def getName(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+
+class Edge(object):
+    def __init__(self, src, dest):
+        self.src = src
+        self.dest = dest
+
+    def getSource(self):
+        return self.src
+
+    def getDestination(self):
+        return self.dest
+
+    def __str__(self):
+        return '{0}->{1}'.format(self.src, self.dest)
+
+
+class Digraph(object):
+    """
+    A directed graph
+    """
+    def __init__(self):
+        # A Python Set is basically a list that doesn't allow duplicates.
+        # Entries into a set must be hashable (where have we seen this before?)
+        # Because it is backed by a hashtable, lookups are O(1) as opposed to the O(n) of a list (nifty!)
+        # See http://docs.python.org/2/library/stdtypes.html#set-types-set-frozenset
+        self.nodes = set([])
+        self.edges = {}
+
+    def addNode(self, node):
+        if node in self.nodes:
+            # Even though self.nodes is a Set, we want to do this to make sure we
+            # don't add a duplicate entry for the same node in the self.edges list.
+            raise ValueError('Duplicate node')
+        else:
+            self.nodes.add(node)
+            self.edges[node] = []
+
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not(src in self.nodes and dest in self.nodes):
+            raise ValueError('Node not in graph')
+        self.edges[src].append(dest)
+
+    def childrenOf(self, node):
+        return self.edges[node]
+
+    def hasNode(self, node):
+        return node in self.nodes
+
+    def __str__(self):
+        res = ''
+        for k in self.edges:
+            for d in self.edges[str(k)]:
+                res = '{0}{1}->{2}\n'.format(res, k, d)
+        return res[:-1]
+
+
+class WeightedDigraph(Digraph):
+    def __init__(self):
+        Digraph.__init__(self)
+
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not(src in self.nodes and dest in self.nodes):
+            raise ValueError('Node not in graph')
+        self.edges[src].append([dest, edge])
+
+    def childrenOf(self, node):
+        return self.edges[node]
+        #return [i for i, _ in self.edges[node]]
+
+    def __str__(self):
+        res = ''
+        for k in self.edges:
+            for d, edge in self.edges[k]:
+                res = '{0}{1}->{2} ({3}, {4})\n'.format(res, k, d, edge.getTotalDistance(), edge.getOutdoorDistance())
+        return res[:-1]
+
+
+class WeightedEdge(Edge):
+    def __init__(self, src, dest, total_distance, outdoor_distance):
+        Edge.__init__(self, src, dest)
+        self.total_distance = float(total_distance)
+        self.outdoor_distance = float(outdoor_distance)
+
+    def getTotalDistance(self):
+        return self.total_distance
+
+    def getOutdoorDistance(self):
+        return self.outdoor_distance
+
+    def __str__(self):
+        return '{0} ({1}, {2})'.format(Edge.__str__(self), self.total_distance, self.outdoor_distance)
+
+    def __repr__(self):
+        return '({0}, {1})'.format(self.total_distance, self.outdoor_distance)
